605c013fa54b3ed932f583eada46d70b83b78904
wollfxp <wollfxp@gmail.com>
2019-02-02 15:14:42 +0200

    separate api security

--- src/main/java/com/dataart/project1/controller/api/HangarApi.java
+++ src/main/java/com/dataart/project1/controller/api/HangarApi.java
@@ -3,6 +3,7 @@ package com.dataart.project1.controller.api;
 import com.dataart.project1.entity.Starship;
 import com.dataart.project1.entity.User;
 import com.dataart.project1.repo.StarshipRepo;
+import com.dataart.project1.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -17,15 +19,18 @@ import java.util.List;
 public class HangarApi {
 
     private StarshipRepo starshipRepo;
+    private UserRepo userRepo;
 
     @Autowired
-    public HangarApi(StarshipRepo starshipRepo) {
+    public HangarApi(StarshipRepo starshipRepo, UserRepo userRepo) {
         this.starshipRepo = starshipRepo;
+        this.userRepo = userRepo;
     }
 
     @GetMapping("/ships")
     @PreAuthorize("hasAuthority('APIUSER')")
-    public List<Starship> getUserShips(@RequestParam("user") User user) {
+    public List<Starship> getUserShips(Principal principal) {
+        User user = userRepo.findByName(principal.getName());
         return starshipRepo.findAllByOwner(user);
     }
 
--- src/main/java/com/dataart/project1/controller/ui/LoginController.java
+++ src/main/java/com/dataart/project1/controller/ui/LoginController.java
@@ -1,4 +1,4 @@
-package com.dataart.project1.controller.api;
+package com.dataart.project1.controller.ui;
 
 import com.dataart.project1.entity.SecurityUser;
 import com.dataart.project1.entity.User;
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletException;
