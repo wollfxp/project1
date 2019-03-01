14166e3b3f653744a1f7c10922cd4667d42f427a
wollfxp <wollfxp@gmail.com>
2019-03-01 18:21:22 +0200

    more kuberton preparations, stateful db

--- src/main/resources/_data.sql
+++ src/main/resources/_data.sql
@@ -6,8 +6,6 @@ space.users
   CONVERT TO CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
 
-select * from users;
-select * from mission;
 
 insert into users (id, ai_cores, credits, hangar_size, name, premium)
 values (1, 10, 15000, 10, 'admin', false);
